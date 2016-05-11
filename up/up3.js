8906b8cf9dc0afb1aa755d9bb0bc9573f17a41a8
lue309 <lue309@newschool.edu>
2016-05-11 16:06:18 -0400

    u

--- up/up3.js
+++ up/up3.js
@@ -141,21 +141,5 @@ jQuery(document).ready(function($){
 });
 
 
-//credits http://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
-jQuery.fn.putCursorAtEnd = function() {
-	return this.each(function() {
-    	// If this function exists...
-    	if (this.setSelectionRange) {
-      		// ... then use it (Doesn't work in IE)
-      		// Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
-      		var len = $(this).val().length * 2;
-      		this.focus();
-      		this.setSelectionRange(len, len);
-    	} else {
-    		// ... otherwise replace the contents with itself
-    		// (Doesn't work in Google Chrome)
-      		$(this).val($(this).val());
-    	}
-	});
-  
+
 };
